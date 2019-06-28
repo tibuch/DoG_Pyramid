da910921dd34d2f272861870adf7f5b741fcb7f7
tibuch <tibuch@mpi-cbg.de>
2019-06-28 10:03:39 +0200

    Remove simplified io dependency

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,13 @@
 
 import net.imagej.ImageJ;
-import net.imagej.ImgPlus;
-import sc.fiji.simplifiedio.SimplifiedIO;
 
-public class Main {
+public class Main
+{
 
-	public static void main(String[] args) {
+	public static void main( String[] args )
+	{
 		final ImageJ ij = new ImageJ();
-		ij.launch(args);
-		final ImgPlus image = SimplifiedIO.openImage("https://imagej.net/images/blobs.gif");
-		ij.ui().show(image);
+		ij.launch( args );
 	}
 
 }
