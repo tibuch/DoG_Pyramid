34c5a525b95a4332d52d73255dac55b46f2d2d12
tibuch <tibuch@mpi-cbg.de>
2019-07-10 17:08:15 +0200

    Pre-match dog-op

--- src/main/java/DoG.java
+++ src/main/java/DoG.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.util.List;
 
+import net.imagej.ops.Op;
+import net.imagej.ops.Ops;
+import net.imagej.ops.special.computer.UnaryComputerOp;
 import org.scijava.ItemIO;
 import org.scijava.command.Command;
 import org.scijava.log.LogService;
@@ -15,60 +18,61 @@ import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.view.Views;
 
-@Plugin( type = Command.class, headless = true, menuPath = "Experiments>Blob Detection>DoG Pyramid" )
-public class DoG< T extends RealType< T > > implements Command
-{
+@Plugin(type = Command.class, headless = true, menuPath = "Experiments>Blob Detection>DoG Pyramid")
+public class DoG<T extends RealType<T>> implements Command {
 
-	@Parameter
-	private LogService log;
+    @Parameter
+    private LogService log;
 
-	@Parameter
-	private OpService ops;
+    @Parameter
+    private OpService ops;
 
-	@Parameter( type = ItemIO.INPUT )
-	private Img< T > image;
+    @Parameter(type = ItemIO.INPUT)
+    private Img<T> image;
 
-	@Parameter( type = ItemIO.INPUT )
-	private double sigma = 2;
+    @Parameter(type = ItemIO.INPUT)
+    private double sigma = 2;
 
-	@Parameter( type = ItemIO.INPUT )
-	private double factor = 1.6;
+    @Parameter(type = ItemIO.INPUT)
+    private double factor = 1.6;
 
-	@Parameter( type = ItemIO.INPUT )
-	private int levels = 5;
+    @Parameter(type = ItemIO.INPUT)
+    private int levels = 5;
 
-	@Parameter( type = ItemIO.OUTPUT )
-	private Img< T > output;
+    @Parameter(type = ItemIO.OUTPUT)
+    private Img<T> output;
 
-	@Override
-	public void run()
-	{
-		final Img< FloatType > converted = ops.convert().float32( image );
-		final Img< FloatType > dog_img = ops.create().img( converted );
+    @Override
+    public void run() {
+        final Img<FloatType> converted = ops.convert().float32(image);
+        final Img<FloatType> dog_img = ops.create().img(converted);
 
-		// Create a NormalizeScaleRealTypes op
-		final RealTypeConverter< FloatType, T > scale_op = ( RealTypeConverter< FloatType, T > ) ops
-				.op( "convert.normalizeScale", dog_img.firstElement(), image.firstElement() );
+        // Create a NormalizeScaleRealTypes op
+        final RealTypeConverter<FloatType, T> scale_op = (RealTypeConverter<FloatType, T>) ops
+                .op("convert.normalizeScale", dog_img.firstElement(), image.firstElement());
 
-		final List< Img< T > > dog = new ArrayList<>();
+        final List<Img<T>> dog = new ArrayList<>();
 
-		double s1 = sigma;
-		double s2 = s1 * factor;
-		for ( int i = 0; i < levels; i++ )
-		{
-			log.info( "Run DoG with sigmas: " + s1 + ", " + s2 );
+        double s1 = sigma;
+        double s2 = s1 * factor;
 
-			ops.filter().dog( dog_img, converted, s1, s2 );
 
-			final Img< T > out = ( Img< T > ) ops.create().img( image );
-			ops.convert().imageType( out, dog_img, scale_op );
-			dog.add( out );
+        for (int i = 0; i < levels; i++) {
+            log.info("Run DoG with sigmas: " + s1 + ", " + s2);
 
-			s1 = s2;
-			s2 = s1 * factor;
-		}
+            // This doesn't make sense in this case, because we have to set s1 and s2 in every iteration.
+            final UnaryComputerOp dog_op = (UnaryComputerOp) ops.op("filter.dog", dog_img, converted, s1, s2);
+            dog_op.compute(converted, dog_img);
 
-		output = ImgView.wrap( Views.stack( dog ), image.factory() );
-	}
+            final Img<T> out = (Img<T>) ops.create().img(image);
+            ops.convert().imageType(out, dog_img, scale_op);
+            dog.add(out);
+
+            s1 = s2;
+            s2 = s1 * factor;
+        }
+
+        output = ImgView.wrap(Views.stack(dog), image.factory());
+    }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,11 @@
 
 import net.imagej.ImageJ;
 
-public class Main
-{
+public class Main {
 
-	public static void main( String[] args )
-	{
-		final ImageJ ij = new ImageJ();
-		ij.launch( args );
-	}
+    public static void main(String[] args) {
+        final ImageJ ij = new ImageJ();
+        ij.launch(args);
+    }
 
 }
